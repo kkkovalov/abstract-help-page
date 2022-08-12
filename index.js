d18bc9303bf6eac65db89064ad3f0374b5f19ce1
Vladyslav Kovalov <56158536+kkkovalov@users.noreply.github.com>
2022-08-12 14:26:58 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function myFunction(){
+    var show = document.getElementById("links");
+    if(show.style.display === "block")
+        show.style.display = "none";
+        else show.style.display = "block";
+}
